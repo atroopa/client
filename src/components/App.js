c49944f54ee1f98a4caec90b696be839ab203a8b
Atroopa <atroopa@gmail.com>
2023-04-14 12:41:49 +0430

    make pagaes Rout b have error

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 
+const pageOne = () => {
+  return(
+    <div>
+      page one
+    </div>
+  );
+}
+
+
+const pageTwo = () => {
+  return(
+    <div>
+      page two
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-      App
+      <BrowserRouter>
+        <Route path='/' exact Component={pageOne} />
+        <Route path='/about' Component={pageTwo} />
+      </BrowserRouter>
     </div>
-  );
+  )
 };
 
 export default App;
