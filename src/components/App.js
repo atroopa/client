218cd240dd2eb66aa739c5ed18bec296ba569160
Atroopa <atroopa@gmail.com>
2023-04-15 18:19:19 +0430

    start to learn react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,13 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
-const pageOne = () => {
-  return(
-    <div>
-      page one
-    </div>
-  );
-}
-
-
-const pageTwo = () => {
-  return(
-    <div>
-      page two
-    </div>
-  );
-}
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Route path='/' exact Component={pageOne} />
-        <Route path='/about' Component={pageTwo} />
-      </BrowserRouter>
+      App
     </div>
-  )
-};
+  );
+}
 
 export default App;
