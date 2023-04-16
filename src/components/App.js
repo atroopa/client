01cc7f5ce8f8536c9fce5bf707ff064269acac67
Atroopa <atroopa@gmail.com>
2023-04-16 14:24:32 +0430

    create stream lists components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
+
+const PageOne = () => <div>Home</div>;
+const PageTwo = () => <div>About</div>;
+const PageThree = () => <div>Contact</div>;
+
+
 
 
 function App() {
   return (
     <div>
-      App
+
+      <p className='px-20 text-blue-700 text-4xl font-sans'>
+        <Link to= "/">Home</Link>
+        &nbsp; &nbsp; &nbsp;
+        <Link to= "/about">About</Link>
+        &nbsp; &nbsp; &nbsp;
+        <Link to= "/contact">Contact</Link>
+        &nbsp; &nbsp; &nbsp;
+      </p>
+    
+      <Routes>
+        <Route exact path='/' element={<PageOne/>} />
+        <Route path='/about' element={<PageTwo/>} />
+        <Route path='/contact' element={<PageThree/>} />
+      </Routes>
     </div>
   );
 }
